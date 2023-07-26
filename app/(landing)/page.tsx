43dab3af1ddc4741560d9988c85d47b4704b1d02
ArnaudBand <bandonkeyea@gmail.com>
2023-07-26 21:16:21 +0300

    Add sign in and sing up button

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
     <div>
       Landing Page (Unprotected)
+
+      <div>
+        <Link href="/sign-in">
+          <Button>Sign in</Button>
+        </Link>
+      </div>
+      <div>
+        <Link href="/sign-up">
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
+
     </div>
   );
 }
