e82210ef1ee2d27b72b1225a6255c6177f567878
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-11-10 15:39:00 +0000

    Add Sidebar component to project

---
+++ components/sidebar.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+export default function Sidebar (){
+  return (
+    <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className='flex items-center pl-3 mb-14'>
+          <div className="relative w-8 h-8 mr-4">
+            <Image
+              fill
+              alt='Logo'
+              src={'/logo.png'}
+              layout="fill"
+              className='object-contain'
+            />
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
